472efd5a20d3519f56462107809d47f1c9bc851b
Zubair Desai <zubairdesai@Zubairs-MacBook-Pro-2.local>
2015-06-30 14:48:17 -0700

    Edit puppies and Create new puppies

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -4,9 +4,47 @@ class PuppiesController < ApplicationController
   def index
   	@puppies = Puppy.all
   end
+  
+  def new
+  	@puppy = Puppy.new
+  end
+
+  def create
+  	puts puppy_params
+  	puppy = Puppy.create(puppy_params)
+  	if puppy
+  	  redirect_to index
+  	end
+  end
+
+  def edit
+  	@puppy = Puppy.find(params[:id])
+  end
 
   def show
   	@puppy = Puppy.find_by(id: params[:id])
+  end
+
+  def update
+  	@puppy = Puppy.find_by(id: params[:id])
+  	@puppy.update(puppy_params)
+  	if @puppy.save
+  		redirect_to puppy_path(@puppy)
+  	else
+  		render :edit
+  	end
+  end
+
+private
+
+  def puppy_params
+  	params.require(:puppy).permit(
+  		:name,
+  		:age,
+  		:bio,
+  		:breed,
+  		:image_url
+  		)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
   root 'puppies#index'
+  get  'puppies/' => 'puppies#index'
+  post 'puppies/' => 'puppies#create'
+  get  'puppies/new' => 'puppies#new'
+  get  'puppies/:id/edit' => 'puppies#edit', as: 'edit_puppy'
   get  'puppies/:id' => 'puppies#show', as: 'puppy'
+  patch 'puppies/:id' => 'puppies#update'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
